bd29de89405a1a4a5895d26a457572a5b63bfe49
patr0335 <patr0335@stud.kea.dk>
2021-03-10 13:27:48 +0100

    til mads

--- src/main/java/HomeController.java
+++ src/main/java/HomeController.java
@@ -8,4 +8,10 @@ public class HomeController {
     public String RenderIndex(){
       return "index";
     }
+
+//    @GetMapping("/submit") {
+//        public String renderSubmit() {
+//            return ""
+//        }
+//    }
 }
